f4d12d4b7a326732ba574ffe414644c4cc1b653e
LeighBarnett <leighbarnett5@gmail.com>
2017-07-09 11:33:51 +0300

    states added

--- public/app.js
+++ public/app.js
@@ -1,5 +1,17 @@
 var app = angular.module('scotcha', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
 
+ $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: 'partial-home.html'
+  })
+
+
+ $stateProvider.state('contact', {
+    url: '/contact',
+    templateUrl: 'partial-contact.html'
+  })
 }); 
+ 
