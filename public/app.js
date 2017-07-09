77c6acf41871d73bdd09a02f9e8b074b84464054
LeighBarnett <leighbarnett5@gmail.com>
2017-07-09 13:51:14 +0300

    main proj done

--- public/app.js
+++ public/app.js
@@ -1,17 +1,39 @@
 var app = angular.module('scotcha', ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/home');
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
 
- $stateProvider.state('home', {
-    url: '/home',
-    templateUrl: 'partial-home.html'
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider.state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: 'HomeListCtrl'
+        })
+.state('home.about', {
+            url: '/about',
+            templateUrl: 'partial-home-about.html',
+            controller: 'HomeListCtrl'
+        })
+
+    $stateProvider.state('contact', {
+        url: '/contact',
+        templateUrl: 'partial-contact.html'
+    })
+
+    $stateProvider.state('scotch', {
+    url: '/scotch/:type?temp',
+    templateUrl: 'partial-scotch.html',
+    controller: function($scope, $stateParams) {
+      $scope.myScotch = $stateParams;
+    }
   })
+});
 
-
- $stateProvider.state('contact', {
-    url: '/contact',
-    templateUrl: 'partial-contact.html'
-  })
-}); 
- 
+app.controller('HomeListCtrl', function($scope){
+	$scope.scotches=['Balvenie', 'The Balmore', 'Highland Park'];
+})
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
+app.all('*', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+})
 
 app.listen(8000, function() {
   console.log("what do you want from me! get me on 8000 ;-)");
